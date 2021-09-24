30d062a2fba74145e5c7b98d9f294b8907cae303
Christoph <christoph.saile@googlemail.com>
2021-09-24 11:28:56 +0200

    feat: add legend

--- src/elements/header/header.ts
+++ src/elements/header/header.ts
@@ -8,8 +8,23 @@ export default function Header(config: Config): string {
   const title = config.title;
   const legend = config.options?.legend;
 
-  return `
-    ${title}
-    ${legend ? 'legend' : 'nolegend'}
+  const renderLegend = () => {
+    return `
+      <section class='houdini__legend'>
+        ${config.data.datasets
+          .map(
+            (elem) =>
+              `<div data-set='${elem.name}'class='houdini__legend-item'><span style='background-color: ${elem.color}'></span><p>${elem.name}</p></div>`
+          )
+          .join('')}
+      </section>
     `;
+  };
+
+  return `
+    <section class='houdini__header'>
+      <h2 class='houdini__title'>${title}</h2>
+      ${legend && renderLegend()}
+    </section>
+  `;
 }
