9735bb552ad82052c1a996c5e1a8c39de49cb58c
Christoph <christoph.saile@googlemail.com>
2021-09-24 21:24:44 +0200

    feat: add radar grid

--- src/charts/radar-chart/radar-chart.ts
+++ src/charts/radar-chart/radar-chart.ts
@@ -1,3 +1,6 @@
+import { niceScale } from '../../utils/nice-num';
+import { getMinValue } from '../../utils/get-min-value';
+import { getMaxValue } from '../../utils/get-max-value';
 import { flattenDataset } from '../../utils/flatten-dataset';
 
 // styles
@@ -14,10 +17,42 @@ class RadarChart {
     this.init();
   }
 
+  private scaleSettings = this.config.data.scale;
   private datasets = this.config.data.datasets;
   private labels = this.config.data.labels;
+  private options = this.config.options;
+
+  private autoScale = this.scaleSettings.auto;
+  private min = this.autoScale
+    ? getMinValue(flattenDataset(this.datasets))
+    : this.scaleSettings.min!;
+  private max = this.autoScale
+    ? getMaxValue(flattenDataset(this.datasets))
+    : this.scaleSettings.max!;
+  private niceNumbers = niceScale(this.min, this.max);
+  private range = this.niceNumbers.niceMaximum - this.niceNumbers.niceMinimum;
+  private segments =
+    (this.niceNumbers.niceMaximum - this.niceNumbers.niceMinimum) / this.niceNumbers.tickSpacing;
+
+  private numberOfLabels = () => {
+    let length;
+    if (this.labels.length < 3) {
+      length = 3;
+    } else if (this.labels.length > 10) {
+      length = 10;
+    } else {
+      length = this.labels.length;
+    }
+    return length;
+  };
+
+  private gridColor = this.options?.gridColor ? this.options.gridColor : '#ccc';
 
   private init = () => {
+    // console.log('min, max', this.min, this.max);
+    // console.log('range', this.range);
+    // console.log('segments', this.segments);
+
     this.render();
   };
 
@@ -38,15 +73,20 @@ class RadarChart {
 
   private renderChart = () => {
     return `
-      <section class='houdini__chart'>
+      <section class='houdini__chart' style='${this.setGridStyle()}'>
         ${this.renderLabels()}
         ${this.renderScale()}
       </section>
     `;
   };
 
+  private setGridStyle = () => {
+    return `background: paint(grid-radar); --grid-labels:${this.numberOfLabels()}; --grid-segments:${
+      this.segments
+    }; --grid-color: ${this.gridColor}`;
+  };
+
   private renderLabels = () => {
-    console.log(this.labels.length);
     return `
       <div class='houdini__labels'>
         ${this.labels}
--- src/worklets/grid-radar.js
+++ src/worklets/grid-radar.js
@@ -1,9 +1,78 @@
 if (typeof registerPaint !== 'undefined') {
   class GridRadar {
     static get inputProperties() {
-      return ['--grid-color', '--grid-segementsY', '--grid-segementsX', '--grid-highlight'];
+      return ['--grid-labels', '--grid-segments', '--grid-color'];
     }
-    paint(ctx, size, properties) {}
+    paint(ctx, size, properties) {
+      const color = String(properties.get('--grid-color'));
+      const segments = parseInt(properties.get('--grid-segments'));
+      const labels = parseInt(properties.get('--grid-labels'));
+
+      const height = size.height;
+      const width = size.width;
+
+      const circleSize = width / 2;
+      const centerX = width / 2;
+      const centerY = height / 2;
+
+      const getPolygonPos = (size, labels) => {
+        let dotsArray = [];
+        let angle = (Math.PI * 2) / labels;
+
+        for (let i = 0; i < labels; i++) {
+          let curPos = {};
+          curPos.x = size * Math.sin(i * angle) + centerX;
+          curPos.y = -size * Math.cos(i * angle) + centerY;
+          dotsArray.push(curPos);
+        }
+
+        // add frist value double to finish the shape
+        dotsArray.push(dotsArray[0]);
+
+        return dotsArray;
+      };
+
+      let radiusSizes = [];
+      let segmentDistance = circleSize / (segments - 1);
+
+      for (let i = 0; i < segments; i++) {
+        radiusSizes[i] = segmentDistance * i;
+      }
+
+      radiusSizes.reverse();
+
+      ctx.strokeStyle = color;
+
+      // Polygon
+      for (let i = 0; i < radiusSizes.length; i++) {
+        const polygon = getPolygonPos(radiusSizes[i], labels);
+        ctx.beginPath();
+        ctx.moveTo(polygon[0].x, polygon[0].y);
+
+        // increase counter + 1 because of doubled first value
+        for (let j = 0; j < labels + 1; j++) {
+          ctx.lineTo(polygon[j].x, polygon[j].y);
+        }
+
+        ctx.stroke();
+      }
+
+      // Axis lines
+      const polygon = getPolygonPos(circleSize, labels);
+      ctx.beginPath();
+
+      for (let i = 0; i < labels; i++) {
+        ctx.moveTo(centerX, centerY);
+        ctx.lineTo(polygon[i].x, polygon[i].y);
+      }
+
+      ctx.stroke();
+
+      // Test Circle
+      ctx.beginPath();
+      ctx.arc(centerX, centerX, circleSize, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
   }
   registerPaint('grid-radar', GridRadar);
 }
