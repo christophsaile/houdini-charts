8b700efff30481ba655724ca0cb19745b9625ffd
Christoph <christoph.saile@googlemail.com>
2021-11-21 15:48:07 +0100

    chore: rename variables

--- src/utils/get-radar-points-.ts
+++ src/utils/get-radar-points-.ts
@@ -5,22 +5,26 @@ import { Coordinates, Range } from '../charts/charts';
  *for css add center
  *for canvas add center + invert y value
  */
-export function getRadarPoints(points: number[], size: Coordinates, range: Range): Coordinates[] {
+export function getRadarPoints(
+  dataseries: number[],
+  size: Coordinates,
+  range: Range
+): Coordinates[] {
   let dotsArray: Coordinates[] = [];
   const center = size.y / 2;
-  const angle = (Math.PI * 2) / points.length;
+  const angle = (Math.PI * 2) / dataseries.length;
 
-  const getPolygonPos = (size: number, dataPoint: number) => {
+  const getPolygonPos = (radiusSize: number, axis: number) => {
     let curPos: Coordinates = { x: 0, y: 0 };
-    curPos.x = size * Math.sin(dataPoint * angle);
-    curPos.y = size * Math.cos(dataPoint * angle);
+    curPos.x = radiusSize * Math.sin(axis * angle);
+    curPos.y = radiusSize * Math.cos(axis * angle);
 
     return curPos;
   };
 
-  for (let i = 0, n = points.length; i < n; i++) {
-    const newSize = center * (points[i] / range.y! - range.zeroY!);
-    dotsArray.push(getPolygonPos(newSize, i));
+  for (let i = 0, n = dataseries.length; i < n; i++) {
+    const radiusSize = center * (dataseries[i] / range.y! - range.zeroY!);
+    dotsArray.push(getPolygonPos(radiusSize, i));
   }
 
   // push first value again to close shape
