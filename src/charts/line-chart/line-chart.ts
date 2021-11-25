95066ab05a7673aeeb4bc4529f24fb9153a87d8f
Christoph <christoph.saile@googlemail.com>
2021-11-25 16:23:02 +0100

    feat: upload test

--- src/charts/line-chart/line-chart.ts
+++ src/charts/line-chart/line-chart.ts
@@ -23,10 +23,14 @@ import Accessibility from '../../elements/accessibility/accessibility';
 // worklets
 const gridLineWorklet = new URL('../../worklets/grid-line.js', import.meta.url);
 const pathLineWorklet = new URL('../../worklets/path-line.js', import.meta.url);
+// @ts-ignore
+CSS.paintWorklet.addModule(gridLineWorklet.href);
+// @ts-ignore
+CSS.paintWorklet.addModule(pathLineWorklet.href);
 
 class LineChart {
   constructor(private readonly root: HTMLElement, private readonly config: Config) {
-    this.loadWorklets();
+    // this.loadWorklets();
     this.init();
   }
 
